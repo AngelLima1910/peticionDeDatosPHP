d3f411bc0c47e29787eec4d98311f804f1224abd
AngelLima1910 <angellima200@hotmail.com>
2023-05-30 19:18:23 -0600

    'ProyectoActualizado'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { articulo } from './interfaces/articulos.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'peticiones';
+    public articulos : articulo[] = [];
+    constructor(private http: HttpClient) { }
+    ngOnInit() {
+        let url = "http://localhost/frontend/Unidad%205/productos/";
+        this.http.get<articulo[]>(url)
+            .subscribe(
+                resultado => {
+                    this.articulos = resultado;
+                    this.validarDatos = this.articulos.length > 0;
+                    console.log(this.articulos);
+                }
+            );
+    }
+    public validarDatos: boolean = false;
 }
